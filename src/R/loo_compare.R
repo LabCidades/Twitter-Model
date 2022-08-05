9869eaedfc22c713919072c3a2d3522249d21743
Jose Storopoli <jose@storopoli.io>
2022-08-05 06:38:13 -0300

    formatting loo_compare.R

--- src/R/loo_compare.R
+++ src/R/loo_compare.R
@@ -3,15 +3,18 @@ library(dplyr)
 library(here)
 library(loo)
 library(rstan)
-
-deaths_only <- list.files(here("results", "deaths_only"), full.names = TRUE) %>%
-    as_cmdstan_fit()
-rstan_deaths_only <- list.files(here("results", "deaths_only"), full.names = TRUE) %>%
-    read_stan_csv()
-twitter_deaths <- list.files(here("results", "twitter_deaths"), full.names = TRUE) %>%
-    as_cmdstan_fit()
-rstan_twitter_deaths <- list.files(here("results", "twitter_deaths"), full.names = TRUE) %>%
-    read_stan_csv()
+deaths_only <- list.files(here("results", "deaths_only"),
+                               full.names = TRUE) %>%
+        as_cmdstan_fit()
+rstan_deaths_only <- list.files(here("results", "deaths_only"),
+                                     full.names = TRUE) %>%
+        read_stan_csv()
+twitter_deaths <- list.files(here("results", "twitter_deaths"),
+                                  full.names = TRUE) %>%
+        as_cmdstan_fit()
+rstan_twitter_deaths <- list.files(here("results", "twitter_deaths"),
+                                        full.names = TRUE) %>%
+        read_stan_csv()
 
 # LOO-CV
 # Extract pointwise log-likelihood
@@ -20,11 +23,16 @@ rstan_twitter_deaths <- list.files(here("results", "twitter_deaths"), full.names
 log_lik_deaths_only <- extract_log_lik(rstan_deaths_only, merge_chains = FALSE)
 r_eff_deaths_only <- relative_eff(exp(log_lik_deaths_only), cores = 2)
 
-log_lik_twitter_deaths <- extract_log_lik(rstan_twitter_deaths, merge_chains = FALSE)
+log_lik_twitter_deaths <- extract_log_lik(rstan_twitter_deaths,
+                                          merge_chains = FALSE)
 r_eff_twitter_deaths <- relative_eff(exp(log_lik_twitter_deaths), cores = 2)
 
-loo_deaths_only <- loo(log_lik_deaths_only, r_eff = r_eff_deaths_only, cores = 2)
-loo_twitter_deaths <- loo(log_lik_twitter_deaths, r_eff = r_eff_twitter_deaths, cores = 2)
+loo_deaths_only <- loo(log_lik_deaths_only,
+                       r_eff = r_eff_deaths_only,
+                       cores = 2)
+loo_twitter_deaths <- loo(log_lik_twitter_deaths,
+                          r_eff = r_eff_twitter_deaths,
+                          cores = 2)
 
 # Compare Models
 comp <- loo_compare(loo_deaths_only, loo_twitter_deaths)
